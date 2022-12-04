e04f4b5ac0f9a3153401025309eb89742da7b589
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2022-12-03 22:57:56 -0800

    ESLint and Prettier

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import { useState } from "preact/hooks";
+import "./app.css";
+import preactLogo from "./assets/preact.svg";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,7 +17,7 @@ export function App() {
       </div>
       <h1>Vite + Preact</h1>
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -28,5 +28,5 @@ export function App() {
         Click on the Vite and Preact logos to learn more
       </p>
     </>
-  )
+  );
 }
